2b00139907b69ab1f81f523feeae6a999942591e
S <sreediss@8c8590320b64.ant.amazon.com>
2017-11-23 23:10:41 +0530

    relearning spoj

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by sreps on 4/14/2016.
  */
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String temp = br.readLine();
+            if(temp.equals("42")){
+                break;
+            }
+            System.out.println(temp);
+        }
     }
 }
